62ac1f4460ff318865548a49c1f845071deb3f5b
Abraham Livinus <abrahamlivinus@gmail.com>
2024-10-02 05:22:12 -0700

    main_0.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,42 @@
 #!/usr/bin/python3
-""" prime game """
+"""
+Define isWineer function, a solution to the Prime Game problem
+"""
 
 
-def isPrime(x):
-    """ checks if a number is prime """
-    for i in range(2, x):
-        if x % i == 0:
-            return False
-    return True
+def primes(n):
+    """Return list of prime numbers between 1 and n inclusive
+       Args:
+        n (int): upper boundary of range. lower boundary is always 1
+    """
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
 
 
 def isWinner(x, nums):
-    """ prime game """
-    if x < 1 or not nums or nums == []:
+    """
+    Determines winner of Prime Game
+    Args:
+        x (int): no. of rounds of game
+        nums (int): upper limit of range for each round
+    Return:
+        Name of winner (Maria or Ben) or None if winner cannot be found
+    """
+    if x is None or nums is None or x == 0 or nums == []:
         return None
-    r = min(x, len(nums))
-    Maria = 0
-    Ben = 0
-    player = 0
-    for r_i in range(r):
-        if nums[r_i] < 2:
+    Maria = Ben = 0
+    for i in range(x):
+        prime = primes(nums[i])
+        if len(prime) % 2 == 0:
             Ben += 1
-        elif nums[r_i] == 2:
-            Maria += 1
         else:
-            player = True
-            prime_exist = 1
-            n = list(range(2, nums[r_i] + 1))
-            while (prime_exist):
-                prime_exist = 0
-                for i in n:
-                    if (isPrime(i)):
-                        prime_exist = 1
-                        player = not player
-                        n = list(filter(lambda x: x % i != 0, n))
-            if (player):
-                Ben += 1
-            else:
-                Maria += 1
-
+            Maria += 1
     if Maria > Ben:
         return 'Maria'
     elif Ben > Maria:
